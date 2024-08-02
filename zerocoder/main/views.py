4ccdbb6ca2c113cfbcc0851889509ecb3ca9063d
DimarashkA <159241659+DimarashkA@users.noreply.github.com>
2024-08-02 13:29:55 +0300

    add static

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Создайте здесь свои представления.
 def index(request):
-    return render(request, "main\index.html")
+    return render(request, "main/index.html")
 
 def data(request):
-    return render(request, "main\data.html")
+    return render(request, "main/data.html")
 
 def test(request):
     return render(request, "main/test.html")

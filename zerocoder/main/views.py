409b7009f4ed21d94d3548f6a77ac108350562ce
DimarashkA <159241659+DimarashkA@users.noreply.github.com>
2024-08-07 16:18:53 +0300

    add_part1

--- zerocoder/main/views.py
+++ zerocoder/main/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Создайте здесь свои представления.
 def index(request):
--- zerocoder/zerocoder/settings.py
+++ zerocoder/zerocoder/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "main", # Django's app
+    "news", # Django's app
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

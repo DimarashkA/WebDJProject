2de65235731a4969291488bef4830d4d1a500941
DimarashkA <159241659+DimarashkA@users.noreply.github.com>
2024-08-08 13:19:54 +0300

    final

--- zerocoder/news/models.py
+++ zerocoder/news/models.py
@@ -6,6 +6,10 @@ class News_post(models.Model):
 	short_description = models.CharField("Краткое описание новости", max_length=200)
 	text = models.TextField("Новость")
 	pub_date = models.DateTimeField("Дата публикации")
+	author = models.CharField("Имя автора", max_length=50)
+
+	def __str__(self):
+		return self.title
 
 	class Meta:
 		verbose_name = "Новость"
